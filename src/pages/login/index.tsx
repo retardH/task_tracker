a8d772289bfc8e9d376cd8c88b6db9913661bc40
U Hein Htut Aung <heinhtut.aung@kbzbank.com>
2024-08-09 11:29:39 +0630

    login_fix

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,7 @@
 import logo from "@/assets/logo.svg";
 import red from "@/assets/Red Mottif.svg";
 import blue from "@/assets/Blue Mottif.svg";
+import kbz from "@/assets/Logo-Vertical.svg";
 import {
   Form,
   FormControl,
@@ -76,77 +77,107 @@ const LoginPage = () => {
     <main className="flex h-[100dvh] w-full items-center justify-center">
       <img src={red} alt="" className="absolute left-0 top-0 w-[200px]" />
       <img src={blue} alt="" className="absolute bottom-0 right-0 w-[200px]" />
-      <section className="w-[500px] rounded-md border px-6 py-12 shadow-md">
-        <div className="mb-8 flex w-full items-center justify-center gap-3">
-          <img src={logo} alt="" className="w-[50px] rounded-sm" />
-          <h2 className="text-4xl font-bold text-primary">
-            Smart Task Tracker
-          </h2>
-        </div>
-        {login.isError && (
-          <Alert variant="destructive" className="mb-4">
-            <ExclamationTriangleIcon />
-            <AlertTitle>Invalid credentials!</AlertTitle>
-            <AlertDescription>Please try again</AlertDescription>
-          </Alert>
-        )}
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="staffId"
-              render={({ field }) => {
-                return (
-                  <FormItem className="col-span-2">
-                    <FormLabel>Staff Id</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        onChange={(e) =>
-                          field.onChange(e.target.value.replace(/[^\d]/g, ""))
-                        }
-                        placeholder="Type your ID"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => {
-                return (
-                  <FormItem className="col-span-2">
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <div className="relative">
+      <section className="hidden h-full w-1/2 flex-col items-center justify-center gap-4 bg-primary/90 md:flex">
+        <img src={kbz} className="w-[200px]" />
+
+        <h1 className="text-4xl font-bold text-white">Hello Welcome...</h1>
+        <h3 className="font-sans text-4xl font-bold italic text-white">
+          Start tracking your work hours now!
+        </h3>
+      </section>
+      <section className="flex w-full flex-col items-center justify-center md:w-1/2">
+        <div className="w-[500px] rounded-sm border px-10 py-10 shadow-2xl">
+          <div className="mb-2 flex w-full items-center justify-center gap-3">
+            <img src={logo} alt="" className="w-[50px] rounded-sm" />
+
+            <h2 className="text-4xl font-bold text-primary">
+              Smart Task Tracker
+            </h2>
+          </div>
+          {login.isError && (
+            <Alert variant="destructive" className="mb-4">
+              <ExclamationTriangleIcon />
+              <AlertTitle>Invalid credentials!</AlertTitle>
+              <AlertDescription>Please try again</AlertDescription>
+            </Alert>
+          )}
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="staffId"
+                render={({ field }) => {
+                  return (
+                    <FormItem className="col-span-2">
+                      <FormLabel>Staff Id</FormLabel>
+                      <FormControl>
                         <Input
                           {...field}
-                          type={showPassword ? "text" : "password"}
-                          placeholder="Type your password"
+                          onChange={(e) =>
+                            field.onChange(e.target.value.replace(/[^\d]/g, ""))
+                          }
+                          placeholder="Type your ID"
                         />
-                        <button
-                          type="button"
-                          onClick={togglePasswordVisibility}
-                          className="absolute inset-y-0 right-0 flex items-center pr-3 text-sm leading-5"
-                        >
-                          {showPassword ? <EyeClosedIcon /> : <EyeOpenIcon />}
-                        </button>
-                      </div>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                );
-              }}
-            />
-            <SubmitButton loading={login.isPending} className="w-full">
-              Login to continue
-            </SubmitButton>
-          </form>
-        </Form>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => {
+                  return (
+                    <FormItem className="col-span-2">
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input
+                            {...field}
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Type your password"
+                          />
+                          <button
+                            type="button"
+                            onClick={togglePasswordVisibility}
+                            className="absolute inset-y-0 right-0 flex items-center pr-3 text-sm leading-5"
+                          >
+                            {showPassword ? <EyeClosedIcon /> : <EyeOpenIcon />}
+                          </button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <SubmitButton loading={login.isPending} className="w-full">
+                Login to continue
+              </SubmitButton>
+            </form>
+          </Form>
+        </div>
       </section>
+
+      {/* <section className="w-1/2 px-10 py-12 shadow-md">
+        <div className="flex flex-col items-center justify-center px-2">
+          <div className="mb-8 flex w-full items-center justify-center gap-3">
+            <img src={logo} alt="" className="w-[50px] rounded-sm" />
+            <h2 className="text-4xl font-bold text-primary">
+              Smart Task Tracker
+            </h2>
+          </div>
+          {login.isError && (
+            <Alert variant="destructive" className="mb-4">
+              <ExclamationTriangleIcon />
+              <AlertTitle>Invalid credentials!</AlertTitle>
+              <AlertDescription>Please try again</AlertDescription>
+            </Alert>
+          )}
+          
+        </div>
+      </section> */}
     </main>
   );
 };
